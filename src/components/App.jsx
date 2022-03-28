6bb0bf2d95ca4cc884853251e7ad42b94735da00
Eligio16 <50426559+Eligio16@users.noreply.github.com>
2022-03-28 13:41:12 -0500

    shortening the code - Arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,23 @@ import React from "react";
 import Card from "./Card";
 import emojipedia from "../emojipedia";
 
-const createCard = (emojipedia) => (
-  <Card
-    key={emojipedia.id}
-    emoji={emojipedia.emoji}
-    name={emojipedia.name}
-    meaning={emojipedia.meaning}
-  />
-);
-
 const App = () => (
   <div>
     <h1>
       <span>emojipedia</span>
     </h1>
 
-    <dl className="dictionary">{emojipedia.map(createCard)};</dl>
+    <dl className="dictionary">
+      {emojipedia.map((emojipedia) => (
+        <Card
+          key={emojipedia.id}
+          emoji={emojipedia.emoji}
+          name={emojipedia.name}
+          meaning={emojipedia.meaning}
+        />
+      ))}
+      ;
+    </dl>
   </div>
 );
 
